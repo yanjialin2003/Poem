d4695f26592d84784c9ac9acc5b88eeb810e33cf
hjy <963839981@qq.com>
2023-11-02 17:18:00 +0800

    add api doc

--- src/main/java/com/douyuehan/doubao/DoubaoApplication.java
+++ src/main/java/com/douyuehan/doubao/DoubaoApplication.java
@@ -1,6 +1,8 @@
 package com.douyuehan.doubao;
 
 import com.douyuehan.doubao.jwt.JwtAuthenticationFilter;
+import io.github.yedaxia.apidocs.Docs;
+import io.github.yedaxia.apidocs.DocsConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,7 +27,18 @@ public class DoubaoApplication extends SpringBootServletInitializer {
         return registrationBean;
     }
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(DoubaoApplication.class, args);
+//    }
+
     public static void main(String[] args) {
+        DocsConfig config = new DocsConfig();
+        config.setProjectPath("D:\\practice\\doubao_community_backend"); // 项目根目录
+        config.setProjectName("backend"); // 项目名称
+        config.setApiVersion("V1.0");       // 声明该API的版本
+        config.setDocsPath("D:\\practice\\doubao_community_backend"); // 生成API 文档所在目录
+        config.setAutoGenerate(Boolean.TRUE);  // 配置自动生成
+        Docs.buildHtmlDocs(config); // 执行生成文档
         SpringApplication.run(DoubaoApplication.class, args);
     }
 }
